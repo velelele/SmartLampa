741f491bdddc5f543d21807c94781b1d5523eb7d
thever4 <Nikita@TheVer4.ru>
2023-11-12 14:26:19 +0500

    Enabled viewBinding and Kirich1409 VBPD Library

--- app/src/main/java/net/intersvyaz/androiddemo/MainActivity.kt
+++ app/src/main/java/net/intersvyaz/androiddemo/MainActivity.kt
@@ -2,10 +2,15 @@ package net.intersvyaz.androiddemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.viewBinding
+import net.intersvyaz.androiddemo.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding: ActivityMainBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
